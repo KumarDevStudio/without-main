d0625843a3f0a74b1cc3ff518c07b8e717db09ba
sky1412001 <akashjaggi1123@gmail.com>
2024-11-27 11:48:34 +0530

    third

---
+++ src/Navbar.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Navbar = () => {
+  return (
+    <nav style={{ backgroundColor: '#282c34', padding: '1rem' }}>
+    <ul style={{ display: 'flex', listStyle: 'none', gap: '1rem', margin: 0, padding: 0 }}>
+      <li><Link to="/" style={{ color: 'white', textDecoration: 'none' }}>Home</Link></li>
+      <li><Link to="/get" style={{ color: 'white', textDecoration: 'none' }}>Get</Link></li>
+      <li><Link to="/form" style={{ color: 'white', textDecoration: 'none' }}>Form</Link></li>
+
+
+    </ul>
+
+
+  </nav>
+
+  )
+}
+
+export default Navbar
